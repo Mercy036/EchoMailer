b179b41ca215557ce78e674e7264f265cd4b5ef2
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-09-09 04:26:59 +0530

    done for the day

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,12 +1,19 @@
 import Sidebar from "../component/sidebar";
 import React, { useState, useEffect } from "react"; // Imports can be combined
-
+import "../css/dashboard.css";
 function Dashboard() {
 
     return (
-        <div>
-            <Sidebar />
-        </div>
+        <>
+                <Sidebar />
+            <div className="dashboard-page">
+                <h2 className="page-title">DashBoard</h2>
+                <h3 className="page-subtitle">Welcome back! Here's what's happening with your emails.</h3>
+            </div>
+
+
+        </>
+
     );
 }
 
