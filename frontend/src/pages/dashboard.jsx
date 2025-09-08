1d52434f838e2e0b5717693dc874386fb634e2c3
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-09-09 03:52:31 +0530

    Moving files

---
+++ frontend/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../component/sidebar";
+import React, { useState, useEffect } from "react"; // Imports can be combined
+
+function Dashboard() {
+
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    );
+}
+
+export default Dashboard;
